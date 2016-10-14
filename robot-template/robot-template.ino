63248ca5acb82cc6ca33bb9b22678c012dd0aeef
Ethan-Stackable <stackbro@gmail.com>
2016-10-14 17:53:12 -0400

    2nd commit
    minor changes, most comments are unfinished ( just dont trust any of
    this)

--- robot-template/robot-template.ino
+++ robot-template/robot-template.ino
@@ -9,18 +9,22 @@ SoftwareSerial wifi(7, 8);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(blueLedPin, OUTPUT);
+
   pinMode(redLedPin, OUTPUT);
   wifi.begin(baudRate);
   while (!wifi)
-  {   
+  {
+    
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly 
+  // robot config 
   if (isEnabled == true)
   {
-    
+    //while timer for game time 
+    //USER CODE HERE for running 
+    //after game ends return to robo config for robit  
   }
 }
