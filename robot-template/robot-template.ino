e5985023bdc4a781ba7da017c43c2865e96c753c
Raptor 1711 Yellow Laptop <raptors1711@gmail.com>
2016-10-14 20:18:06 -0400

    I made the motors work
    The motors should recieve a val 0-9 and then work

--- robot-template/robot-template.ino
+++ robot-template/robot-template.ino
@@ -2,7 +2,18 @@
 
 int blueLedPin = 13;
 int redLedPin = 14;
-long baudRate;
+long baudRate = 9600;
+//intialize motor pins
+int m1 = 1;
+int m2 = 2;
+int m3 = 3;
+int m4 = 4;
+//intialize motor speed variables
+int m1s = 0;
+int m2s = 0;
+int m3s = 0;
+int m4s = 0;
+int readstate = 1;
 boolean isEnabled;
 SoftwareSerial wifi(7, 8);
 
@@ -11,8 +22,12 @@ void setup() {
   // put your setup code here, to run once:
 
   pinMode(redLedPin, OUTPUT);
+  pinMode(m1,OUTPUT);
+  pinMode(m2,OUTPUT);
+  pinMode(m3,OUTPUT);
+  pinMode(m4,OUTPUT);
   wifi.begin(baudRate);
-  while (!wifi)
+  while (//wifi is not available)
   {
     
   }
@@ -24,6 +39,51 @@ void loop() {
   if (isEnabled == true)
   {
     //while timer for game time 
+    if(!readstate){
+    switch (mySerial.read()){
+    case 'a':
+    readstate = 1;
+    break;
+    case 'b':
+    readstate = 2;
+    break  
+    case 'c':
+    readstate = 3;
+    break;
+    case 'd':
+    readstate = 4;
+    break;
+    default: 
+    //error!!!!
+    break;
+    }
+    }
+    if(readstate != 0){
+      if(readstate == 1&&mySerial.read()!='a'){
+         //create a numeric val for the motor from the char the arduino recieves
+         char mspeedchar = mySerial.read();
+         m1s = map(int(mspeedchar - '0'),0,9,0,255);
+        }
+        if(readstate == 1&&mySerial.read()!='b'){
+         //create a numeric val for the motor from the char the arduino recieves
+         char mspeedchar = mySerial.read();
+         m2s = map(int(mspeedchar - '0'),0,9,0,255);
+        }
+        if(readstate == 1&&mySerial.read()!='c'){
+         //create a numeric val for the motor from the char the arduino recieves
+         char mspeedchar = mySerial.read();
+          m3s = map(int(mspeedchar - '0'),0,9,0,255);
+        }
+        if(readstate == 1&&mySerial.read()!='d'){
+         //create a numeric val for the motor from the char the arduino recieves
+         char mspeedchar = mySerial.read();
+         m4s = map(int(mspeedchar - '0'),0,9,0,255);
+        }
+      }
+      analogWrite(m1,m1s);
+      analogWrite(m2,m2s);
+      analogWrite(m3,m3s);
+      analogWrite(m4,m4s);
     //USER CODE HERE for running 
     //after game ends return to robo config for robit  
   }
